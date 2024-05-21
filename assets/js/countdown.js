516832d2bfa7143805aa032f36a45a95341e941b
MattiaL22 <Mattialotesoriere@gmail.com>
2024-05-21 16:25:29 +0200

    Caricamento Sito

---
+++ assets/js/countdown.js
@@ -0,0 +1,23 @@
+// Definisci la data e l'ora di riferimento per il countdown
+const targetDate = new Date('2024-06-08T14:00:00');
+
+// Funzione per calcolare il countdown
+function countdown() {
+    const currentDate = new Date();
+    const timeDifference = targetDate - currentDate;
+
+    // Calcola i giorni, le ore, i minuti e i secondi rimanenti
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    // Aggiorna il contenuto degli elementi HTML con il countdown
+    document.getElementById('days').textContent = days;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
+
+// Esegui la funzione di countdown ogni secondo
+setInterval(countdown, 1000);
---
+++ assets/js/test.js
@@ -0,0 +1,6 @@
+var logo = document.querySelector('#logofr');
+    function disattiva() {
+        if(logo.style.display =='none'){
+        logo.style.display = "inline-block";}
+     else{  logo.style.display = "none";}
+ }
